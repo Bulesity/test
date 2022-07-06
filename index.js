7df72d12edb05fe0e536340e71e4112195458544
Bulesity <15880912625@163.com>
2022-07-06 10:00:10 +0800

    提交"

---
+++ index.js
@@ -0,0 +1,53 @@
+// let people = [
+//     {name:'Alice',age:21},
+//     {name:'Blice',age:20},
+//     {name:'Clice',age:21},
+// ]
+// let newpeople = people.groupBy(({age})=>age)
+// console.log(newpeople)
+// let inputObj = document.getElementById('input')
+// let spanObj = document.getElementById('span')
+//console.log(spanObj,inputObj)
+// inputObj.onchange = function(){      
+//     spanObj.innerText = this.value
+// }
+//  let str = ''
+//  for(let i = 0;i<10;i++){
+//      for(let j = 0;j<i;j++){
+//          str += '*'
+//      }
+//      str += '</br>'
+//  }
+//document.write(str)
+// function comma(x){
+//     let str = x + ''
+//    if(parseInt(x) == null){
+//        alert(`${x}非整数!`)
+//        return
+//    }
+//    if(x < 1000){
+//        alert('数字必须大于1000！')
+//        return
+//    }
+//    return str.substring(0,str.length-3) + ',' + str.substring(str.length - 4,str.length-1)
+// }
+
+//console.log(comma(12300))
+let app = new Vue({
+    el: '#app',
+    data:{
+          name:'123'  
+    },
+    mounted(){
+
+    },
+    watch:{
+
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+})
